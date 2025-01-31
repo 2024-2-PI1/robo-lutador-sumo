a41d05ce5b0e34cf4c990249e274b86eacb053f8
martinsglucas <martin5.luca5.univ@gmail.com>
2025-01-31 17:25:21 -0300

    (#7): cria funcoes de andar e rotacionar
    Co-authored-by: ArtyMend07 <ArtyMend07@users.noreply.github.com>

--- sketch/main/driver.h
+++ sketch/main/driver.h
@@ -1,34 +1,50 @@
-# define IN1 5;
-# define IN2 6;
-# define IN3 9;
-# define IN4 10;
+# define IN1 5
+# define IN2 6
+# define IN3 9
+# define IN4 10
 
-# define ENA 3;
-# define ENB 4;
+# define ENA 8
+# define ENB 11
 
 void ligarMotores(int velocidade);
+void andarFrente(int velocidade);
+void andarTras(int velocidade);
+void rotacionar(int velocidade);
+void parar();
 /*
 Liga os motores com a velocidade informada, passar
 um inteiro negativo como parámetro faz o robô dar ré
 */
-void ligarMotores(int velocidade) {
-  if (velocidade > 0) {
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, LOW);
-    digitalWrite(IN3, HIGH);
-    digitalWrite(IN4, LOW);
-  } else if (velocidade == 0) {
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, LOW);
-    digitalWrite(IN3, LOW);
-    digitalWrite(IN4, LOW);
-  } else if (velocidade < 0) {
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, HIGH);
-    digitalWrite(IN3, LOW);
-    digitalWrite(IN4, HIGH);
-  }
 
-  analogWrite(ENA, abs(velocidade));
-  analogWrite(ENB, abs(velocidade));
+void andarFrente(int esquerdo, int direito){
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA, esquerdo);
+  analogWrite(ENB, direito);
+}
+void andarTras(int esquerdo, int direito){
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);  
+  analogWrite(ENA, esquerdo);
+  analogWrite(ENB, direito);
+}
+void rotacionar(int velocidade){
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);  
+  analogWrite(ENA, velocidade);
+  analogWrite(ENB, velocidade);
+  // digitalWrite(IN3, LOW);
+  // digitalWrite(IN4, LOW);  
+}
+void parar(){
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);  
 }
