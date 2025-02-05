fb6a6735358616b8889033d8faa588c76cf507d1
martinsglucas <martin5.luca5.univ@gmail.com>
2025-02-05 14:31:50 -0300

    (#7): adiciona velocidade à rotacao e descricao das funcoes
    Co-authored-by: Guilherme Westphall <guilhermewestphall@gmail.com>
    Co-authored-by: Gustavo Kenzo <gustavokenzo1@users.noreply.github.com>

--- sketch/main/driver.h
+++ sketch/main/driver.h
@@ -6,38 +6,41 @@
 # define ENA 8
 # define ENB 11
 
-void ligarMotores(int velocidade);
 void andarFrente(int velocidade);
 void andarTras(int velocidade);
 void rotacionar(int velocidade);
 void parar();
-/*
-Liga os motores com a velocidade informada, passar
-um inteiro negativo como parámetro faz o robô dar ré
-*/
 
+/*
+Faz o robô andar para frente com a velocidade informada
+*/
 void andarFrente(int esquerdo, int direito){
   analogWrite(IN1, direito);
   digitalWrite(IN2, LOW);
   analogWrite(IN3, esquerdo);
   digitalWrite(IN4, LOW);
-  // analogWrite(ENA, esquerdo);
-  // analogWrite(ENB, direito);
 }
+/*
+Faz o robô andar para trás com a velocidade informada
+*/
 void andarTras(int esquerdo, int direito){
   digitalWrite(IN1, LOW);
   analogWrite(IN2, direito);
   digitalWrite(IN3, LOW);
   analogWrite(IN4, esquerdo);  
 }
+/*
+Faz o robô rotacionar com a velocidade informada
+*/
 void rotacionar(int velocidade){
   digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
+  analogWrite(IN2, velocidade);
+  analogWrite(IN3, velocidade);
   digitalWrite(IN4, LOW);  
-  // digitalWrite(IN3, LOW);
-  // digitalWrite(IN4, LOW);  
 }
+/*
+Faz o robô parar
+*/
 void parar(){
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
